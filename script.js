8be4e24ce7957cca5b1550ccacede17de9c68162
vedant mishra <vedantmishra302@gmail.com>
2023-08-13 15:13:20 +0530

    dates.json deleted and dates.js created ACC

--- script.js
+++ script.js
@@ -3,32 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const datePicker = document.getElementById("datePicker");
   const mapFrame = document.getElementById("mapFrame");
 
-  // Load the dates and URLs from the JSON file
-  fetch("dates.json")
-    .then(response => response.json())
-    .then(data => {
-      const availableDates = data.reverse(); // Latest date at the top
+  // Set the minimum and maximum selectable dates
+  datePicker.min = availableDates[availableDates.length - 1].date;
+  datePicker.max = availableDates[0].date;
 
-      // Set the minimum and maximum selectable dates
-      datePicker.min = availableDates[availableDates.length - 1].date;
-      datePicker.max = availableDates[0].date;
+  // Update the map and date display
+  function updateMapAndDate(date) {
+    const mapUrl = availableDates.find((entry) => entry.date === date)?.mapUrl || "";
+    mapFrame.src = mapUrl;
+    currentDateSpan.textContent = date;
+  }
 
-      // Update the map and date display
-      function updateMapAndDate(date) {
-        const mapUrl = availableDates.find((entry) => entry.date === date)?.mapUrl || "";
-        mapFrame.src = mapUrl;
-        currentDateSpan.textContent = date;
-      }
+  // Listen for changes in the date picker
+  datePicker.addEventListener("change", function () {
+    const selectedDate = datePicker.value;
+    updateMapAndDate(selectedDate);
+  });
 
-      // Listen for changes in the date picker
-      datePicker.addEventListener("change", function () {
-        const selectedDate = datePicker.value;
-        updateMapAndDate(selectedDate);
-      });
-
-      // Initialize with the most recent available date
-      const mostRecentDate = availableDates[0].date;
-      updateMapAndDate(mostRecentDate);
-    })
-    .catch(error => console.error("Error loading dates.json:", error));
+  // Initialize with the most recent available date
+  const mostRecentDate = availableDates[0].date;
+  updateMapAndDate(mostRecentDate);
 });
