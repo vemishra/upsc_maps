f659baca0f5c5dd54fb3d8489fcce38440d10b57
vedant mishra <vedantmishra302@gmail.com>
2023-08-13 14:35:05 +0530

    Created dates.json And corresponding changes (ACC)

--- script.js
+++ script.js
@@ -3,34 +3,32 @@ document.addEventListener("DOMContentLoaded", function () {
   const datePicker = document.getElementById("datePicker");
   const mapFrame = document.getElementById("mapFrame");
 
-  // Sample available dates and corresponding map URLs
-  const availableDates = [
-    { date: "2023-08-10", mapUrl: "https://www.google.com/maps/d/u/0/embed?mid=1tkMwfnKgjAeIrvZmz6w3fRSHUZxnR5I&ehbc=2E312F" },
-    { date: "2023-08-11", mapUrl: "https://www.google.com/maps/d/u/0/embed?mid=1tkMwfnKgjAeIrvZmz6w3fRSHUZxnR5I&ehbc=2E312F" },
-    // Add more dates and URLs as needed
-  ];
+  // Load the dates and URLs from the JSON file
+  fetch("dates.json")
+    .then(response => response.json())
+    .then(data => {
+      const availableDates = data.reverse(); // Latest date at the top
 
-  // Get today's date in YYYY-MM-DD format
-  const todayDate = new Date().toISOString().split("T")[0];
+      // Set the minimum and maximum selectable dates
+      datePicker.min = availableDates[availableDates.length - 1].date;
+      datePicker.max = availableDates[0].date;
 
-  // Set the minimum and maximum selectable dates
-  datePicker.min = availableDates[0].date;
-  datePicker.max = availableDates[availableDates.length - 1].date;
+      // Update the map and date display
+      function updateMapAndDate(date) {
+        const mapUrl = availableDates.find((entry) => entry.date === date)?.mapUrl || "";
+        mapFrame.src = mapUrl;
+        currentDateSpan.textContent = date;
+      }
 
-  // Update the map and date display
-  function updateMapAndDate(date) {
-    const mapUrl = availableDates.find((entry) => entry.date === date)?.mapUrl || "";
-    mapFrame.src = mapUrl;
-    currentDateSpan.textContent = date;
-  }
+      // Listen for changes in the date picker
+      datePicker.addEventListener("change", function () {
+        const selectedDate = datePicker.value;
+        updateMapAndDate(selectedDate);
+      });
 
-  // Listen for changes in the date picker
-  datePicker.addEventListener("change", function () {
-    const selectedDate = datePicker.value;
-    updateMapAndDate(selectedDate);
-  });
-
-  // Initialize with the most recent available date
-  const mostRecentDate = availableDates[availableDates.length - 1].date;
-  updateMapAndDate(mostRecentDate);
+      // Initialize with the most recent available date
+      const mostRecentDate = availableDates[0].date;
+      updateMapAndDate(mostRecentDate);
+    })
+    .catch(error => console.error("Error loading dates.json:", error));
 });
